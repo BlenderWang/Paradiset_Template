6803aff431b8039c1dc73c9806480293a0e59e9b
maokei <rickard.joh@gmail.com>
2019-01-17 22:24:20 +0100

    about template

---
+++ jagarparadiset/about.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Om Johannes
+*/
+get_header();
+?>
+    <main role="main">
+        <div class="container">
+            <h1 class='text-center text-light'>Om Johannes</h1>
+            <img src="./images/JC oktober.jpg" alt="" class="om-img">
+
+            <!-- <div class="card card-om"> -->
+                <div class="card-body bg-light">
+                    <h5 class="card-title display-6">Om Johannes</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, odit incidunt? Iusto odit tempore aut illo ex praesentium esse eligendi! Quisquam, minima. Deserunt dolores animi sequi qui rem dolore iusto?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, odit incidunt? Iusto odit tempore aut illo ex praesentium esse eligendi! Quisquam, minima. Deserunt dolores animi sequi qui rem dolore iusto?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, odit incidunt? Iusto odit tempore aut illo ex praesentium esse eligendi! Quisquam, minima. Deserunt dolores animi sequi qui rem dolore iusto?
+                    </p>
+                </div>
+            <!-- </div> -->
+        </div>
+    </main>
+<?php get_footer();  ?>
